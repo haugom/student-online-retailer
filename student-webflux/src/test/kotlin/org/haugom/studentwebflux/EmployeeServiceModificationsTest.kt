bfcc9b4c76fab9da1ea86850152dcf9f40928893
Geir Haugom <haugom@gmail.com>
2022-06-17 09:24:10 +0200

    fix: Lag test for sletting av employee

--- student-webflux/src/test/kotlin/org/haugom/studentwebflux/EmployeeServiceModificationsTest.kt
+++ student-webflux/src/test/kotlin/org/haugom/studentwebflux/EmployeeServiceModificationsTest.kt
@@ -1,11 +1,15 @@
 package org.haugom.studentwebflux
 
+import mu.KotlinLogging
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest
 import org.springframework.context.annotation.Import
+import org.springframework.util.Assert
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
+
+private val logger = KotlinLogging.logger {}
 
 @DataR2dbcTest
 @Import(EmployeeService::class)
@@ -31,5 +35,38 @@ class EmployeeServiceModificationsTest(
             .create(updatedEmployee)
             .expectNextMatches { emp -> emp.name == "simon the new sorcerer" }
             .verifyComplete()
+    }
+
+    @Test
+    fun `deleteEmployee the employee should be deleted from the database`() {
+        var id: Long = 0
+        // insert one user
+        service.create("test", 1.0, "test").subscribe()
+
+        // insert another user and delete it
+        val deletedEmployee = service.create("Abrahamsen", 2.0, "Oslo")
+            .flatMap { emp ->
+                id = emp.id!!
+                service.delete(emp.id)
+            }
+        StepVerifier
+            .create(deletedEmployee)
+            .expectNextMatches { emp -> emp!!.name == "Abrahamsen" }
+            .verifyComplete()
+
+        logger.info { "id was $id" }
+
+        val byId = service.getById(id)
+        StepVerifier
+            .create(byId)
+            .expectComplete()
+
+        val liste: ArrayList<Employee> = ArrayList()
+        service.getAll().subscribe {
+            liste.add(it)
+        }
+
+        Assert.isTrue(liste.size == 1, "it should only be one employee")
+        Assert.isTrue(liste[0].name == "test", "the only users name should be test")
     }
 }
